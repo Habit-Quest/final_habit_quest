a2a55c970ef8a8a7a7b92b9e694773be5d017d02
Jade Miller <mil19017@byui.edu>
2025-04-03 22:23:03 -0600

    buttons fixed

--- scripts/fail_animation.js
+++ scripts/fail_animation.js
@@ -3,13 +3,10 @@ fail_button.addEventListener("click", fail);
 
 let succeed_button = document.getElementById("finishedAnimation");
 succeed_button.addEventListener("click", succeed);
-
 let progress_percent = 100;
-// const key
-// on load
+
 
 function fail() {
-// onclick
     let heart_img = document.querySelector(".heart_img");
     let progress_bar = document.querySelector(".progress_bar");
     let flash_animation = document.querySelector(".flash_animation");
@@ -23,8 +20,7 @@ function fail() {
         setTimeout((n) => {
             flash_animation.classList.remove("red_flash");
             heart_img.classList.remove("heart_hurt");
-        }, 100)
-    // in miliseconds
+        }, 1000)
         break
     }
 
@@ -32,11 +28,9 @@ function fail() {
         heart_img.classList.add("heart_sad");
         break
     }
-  
 }
 
 function succeed() {
-// onclick
     fail_button.classList.add("succeed_animation")
 
     while (progress_percent != 100) {
